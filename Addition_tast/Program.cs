030a2b6460ca1b3ec799787631199e7ba0419b4b
Loarlam <messiah02@mail.ru>
2020-04-19 19:56:38 +0300

    Программа + фоточка!

--- Addition_tast/Program.cs
+++ Addition_tast/Program.cs
@@ -2,13 +2,30 @@
 В коллекцию  ArrayList, через вызов метода Add добавьте элементы структурного и ссылочного типа, переберите данную коллекцию с помощью, цикла for – С какой проблемой вы столкнулись? 
 */
 using System;
+using System.Collections;
 
 namespace Addition_tast
 {
+    class MyClass { }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("Строка");
+            arrayList.Add(new MyClass());
+            arrayList.Add(457);
+            arrayList.Add(4.5);
+            arrayList.Add('a');
+            arrayList.Add(6m);
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
