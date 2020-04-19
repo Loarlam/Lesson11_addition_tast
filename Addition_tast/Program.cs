847994f47112c72809c253ba2fb14b7d3bcad2e2
Loarlam <messiah02@mail.ru>
2020-04-19 16:46:15 +0300

    Add project files.

---
+++ Addition_tast/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+В коллекцию  ArrayList, через вызов метода Add добавьте элементы структурного и ссылочного типа, переберите данную коллекцию с помощью, цикла for – С какой проблемой вы столкнулись? 
+*/
+using System;
+
+namespace Addition_tast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
